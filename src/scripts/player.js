4b0cb38d126fe717d0a78d3a89b1d2d53acbd111
Sandra Szenti <alexandraszenti@gmail.com>
2015-06-13 15:24:04 +0100

    adding intel specific xdk and xdke file plus a bit of front end

--- src/scripts/player.js
+++ src/scripts/player.js
@@ -1,9 +1,15 @@
 (function($, window, undefined) {
 	console.log('hello');
 
-	var query = 'http://developer.echonest.com/api/v4/song/search?api_key=7PGUFNFZGMNEOS65Z&mood=happy';
+	var happyQuery = 'http://developer.echonest.com/api/v4/song/search?api_key=7PGUFNFZGMNEOS65Z&mood=happy';
+	
+	// Limit is 20 requests per minute so use wisely!!
+	// Or you can set up own free api keys here: http://developer.echonest.com
 
-	$.get(query, function( data ) {
-		console.log(data);
-	});
+	// $.get(happyQuery, function( data ) {
+	// 	console.log(data);
+	// 	var newSong = data.response.songs[0].artist_name + ' - ' + data.response.songs[0].title;
+	// 	console.log(newSong);
+	// 	$('#song-name').html(newSong);
+	// });
 }(jQuery, window));
